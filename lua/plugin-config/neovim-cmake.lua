752830239bab6c3e3f9b8d69893e996b8684fc35
CQNB <1342434213@qq.com>
2022-07-16 20:10:56 +0800

    定义自己的C++项目模板

--- lua/plugin-config/neovim-cmake.lua
+++ lua/plugin-config/neovim-cmake.lua
@@ -11,12 +11,17 @@ if not status_ then
     return
 end
 
+-- 当前目录
+local script_path = Path:new(debug.getinfo(1).source:sub(2))
+
 cmake.setup({
     cmake_executable = 'cmake', -- CMake executable to run.
     save_before_build = true, -- Save all buffers before building.
     parameters_file = 'neovim.json', -- JSON file to store information about selected target, run arguments and build type.
     build_dir = tostring(Path:new('{cwd}', 'build', '{os}-{build_type}')), -- Build directory. The expressions `{cwd}`, `{os}` and `{build_type}` will be expanded with the corresponding text values. Could be a function that return the path to the build directory.
     --samples_path = tostring(script_path:parent():parent():parent() / 'samples'), -- Folder with samples. `samples` folder from the plugin directory is used by default.
+    -- 项目模板地址这里很奇怪路径一定要这么写 写绝对定位不行
+    samples_path = tostring(script_path:parent():parent():parent() / 'samples'),
     default_projects_path = tostring(Path:new(vim.loop.os_homedir(), 'Projects')), -- Default folder for creating project.
     configure_args = { '-D', 'CMAKE_EXPORT_COMPILE_COMMANDS=1' }, -- Default arguments that will be always passed at cmake configure step. By default tells cmake to generate `compile_commands.json`.
     build_args = {
