ecc6810d20a0ac3d202370262f8064bc0b0875c8
CQNB <1342434213@qq.com>
2022-08-17 14:37:36 +0800

    移除无用代码

--- lua/lsp/config/cpp.lua
+++ lua/lsp/config/cpp.lua
@@ -11,20 +11,6 @@ return {
                 -- client.resolved_capabilities.document_formatting = false --已弃用
                 -- client.resolved_capabilities.document_range_formatting = false --已弃用
 
-                -- 相同单词高亮显示 又是resolved_capabilities这个弃用的东西 服了
-                --if client.resolved_capabilities.document_highlight then
-                --    vim.api.nvim_exec(
-                --        [[
-                --            augroup lsp_document_highlight
-                --                autocmd! * <buffer>
-                --                autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-                --                autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-                --            augroup END
-                --        ]],
-                --        false
-                --    )
-                --end
-
                 local function buf_set_keymap(...)
                     vim.api.nvim_buf_set_keymap(bufnr, ...)
                 end
